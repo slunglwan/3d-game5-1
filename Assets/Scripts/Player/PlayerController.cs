38c01ee5fb01e39d064059af40e8e99c58af690c
gamedojang <gamedojang@gmail.com>
2025-09-29 17:23:56 +0900

    플레이어 입력 시스템 추가

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,27 +1,33 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using static Constants;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
 {
+    // 컴포넌트 캐싱
     private Animator _animator;
+    private PlayerInput _playerInput;
+    
+    // 상태 정보
     public EPlayerState State { get; private set; }
-
     private Dictionary<EPlayerState, IPlayerState> _states;
 
     private void Awake()
     {
+        // 컴포넌트 초기화
         _animator = GetComponent<Animator>();
+        _playerInput = GetComponent<PlayerInput>();
         
+        // 상태 객체 초기화
         var playerStateIdle = new PlayerStateIdle(this, _animator);
-
         _states = new Dictionary<EPlayerState, IPlayerState>
         {
             { EPlayerState.Idle, playerStateIdle },
         };
-        
         // 상태 초기화
         SetState(EPlayerState.Idle);
     }
